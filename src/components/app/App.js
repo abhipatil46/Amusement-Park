0a7067e93f1b8170f21deb114b3d43d04a8b831b
vaishnavi6612 <mundhekarvaishnavi6@gmail.com>
2021-09-16 13:06:47 +0530

    Somethig

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,13 +3,9 @@ import About from '../about/about'
 function App() {
   return (
     <>
-<<<<<<< HEAD
     <h1>Hello</h1>
     <About/>
-
-=======
     <h1>Abhishek Patil</h1>
->>>>>>> 6a068f38a431763437426a7fc3dff4141bb990bd
     </>
   )
 };
