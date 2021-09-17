2d49d4418356f461d0637ae8c359105e6707b9c3
Abhishek <abhicpatil2192000@gmail.com>
2021-09-17 16:23:28 +0530

    Created Offers section,Modified App.css and Index.css

--- src/components/offers/Offers.js
+++ src/components/offers/Offers.js
@@ -3,37 +3,33 @@ import getOffersData from '../offers/OffersData';
 import '../css/Offers.css';
 
 function Offers() {
-    const { offer, setOffer } = useState();
+    let [costomerOffer, setOffer] = useState([]);
+    useEffect(()=>{
+        setOffer(getOffersData());
+    },[]);
+    // let loadOffer=()=>{
+    //     setOffer(getOffersData());
+    // }
     return (
         <>
             <div className="container">
-                <button className="btn btn-primary">Load Offers</button>
-                <div className="offer-div">
-                        <div className="card w-50 mt-3 m-1">
+            <h1 className="text-center">Letest Offers</h1>
+            {/* <button className="btn btn-primary" onClick={loadOffer}>Check Letest Offers</button> */}
+            <div className="offer-div m-3 p-3 row">
+                { costomerOffer.map((item)=>
+                        <div className="card col-5 card-h m-2 ml-5 text-center">
                             <div className="card-body">
-                                <h5 className="card-title">Roller Coster</h5>
-                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" className="btn btn-primary">Button</a>
+                                <h5 className="card-title">{item["name"]}</h5>
+                                <p className="card-text">{item["info"]}</p>
+                                <a href="#" className="btn btn-primary m-2">{item["price"]}</a>
+                                <a href="#" className="btn btn-success m-2">Book Ticket</a>
                             </div>
                         </div>
-                        <div className="card w-50 mt-3 m-1">
-                            <div className="card-body">
-                                <h5 className="card-title">Roller Coster</h5>
-                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" className="btn btn-primary">Button</a>
-                            </div>
-                        </div>
-                        <div className="card w-50 mt-3 m-1">
-                            <div className="card-body">
-                                <h5 className="card-title">Roller Coster</h5>
-                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" className="btn btn-primary">Button</a>
-                            </div>
-                        </div>
-                        </div>
-              </div>          
-        </>
-    )
+                )}   
+                </div>
+            </div>
+    </>
+)
 }
 
-export default Offers
+export default Offers;
--- src/components/offers/OffersData.js
+++ src/components/offers/OffersData.js
@@ -1,21 +1,48 @@
 let offersData=[
     {   
-            id:0,
-        name:"Roller Coster",
-        price:"3000",
-        info:"Roller coster info will come over Here"
+        "id":0,
+        "name":"Roller Coster",
+        "price":"3000",
+        "info":"Roller coster info will come over Here"
     },
-    {   id:1,
-        name:"Adventure Park",
-        price:"2999",
-        info:"Adventure Park info will come over Here"
+    {   "id":1,
+        "name":"Adventure Park",
+        "price":"2999",
+        "info":"Adventure Park info will come over Here"
     },
-    {   id:2,
-        name:"Water Park",
-        price:"1999",
+    {   "id":2,
+        "name":"Water Park",
+        "price":"1999",
+        info:"Water info will come over Here"
+    },
+    {   "id":3,
+        "name":"Water Park",
+        "price":"1999",
+        info:"Water info will come over Here"
+    },
+    {   "id":4,
+        "name":"Water Park",
+        "price":"1999",
+        info:"Water info will come over Here"
+    },
+    {   "id":5,
+        "name":"Water Park",
+        "price":"1999",
+        info:"Water info will come over Here"
+    }
+    ,{   "id":6,
+    "name":"Water Park",
+    "price":"1999",
+    info:"Water info will come over Here"
+},
+{   "id":7,
+        "name":"Water Park",
+        "price":"1999",
         info:"Water info will come over Here"
     }
 ];
 function getOffersDate(){
     return offersData;
 }
+
+export default getOffersDate;
