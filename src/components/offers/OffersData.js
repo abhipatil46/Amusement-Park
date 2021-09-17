f7f209ba13fd869071b7c25b503f897a5b8748bd
Abhishek <abhicpatil2192000@gmail.com>
2021-09-17 10:04:56 +0530

    Working on Offers , Created Required files for team members to use

---
+++ src/components/offers/OffersData.js
@@ -0,0 +1,21 @@
+let offersData=[
+    {   
+            id:0,
+        name:"Roller Coster",
+        price:"3000",
+        info:"Roller coster info will come over Here"
+    },
+    {   id:1,
+        name:"Adventure Park",
+        price:"2999",
+        info:"Adventure Park info will come over Here"
+    },
+    {   id:2,
+        name:"Water Park",
+        price:"1999",
+        info:"Water info will come over Here"
+    }
+];
+function getOffersDate(){
+    return offersData;
+}
