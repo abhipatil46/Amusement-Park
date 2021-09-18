0807096b3b02320aa9f49671c68022a2217f6743
Rekha101998 <rathodrekha733@gmail.com>
2021-09-18 12:56:54 +0530

    created home page design

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,54 +1,47 @@
 import React from 'react'
-import s1 from '../images/slider1.jpg';
+import '../home/home.css'
+import s3 from '../images/slider3.jpg';
+import s4 from '../images/slider4.jpg';
+import s2 from '../images/slider2.jpg';
 function Home() {
     return (
         <div>
-<<<<<<< HEAD
-            <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src={s1} className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+        <div className="carousel m-5">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src={s3} alt="First slide"/>
     </div>
-    
-    <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src={s4} alt="Second slide"/>
     </div>
-    <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src={s2} alt="Third slide"/>
     </div>
   </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
-=======
-            <h1>Hello Home</h1>
->>>>>>> 31f514b2ad47861c2c86bc03250710b0b5708098
-        </div>
+</div>
+
+<div>
+    <p className="m-5">An interesting and fun water park in India, Wonderla is a favourite among young kids as well as older
+         people for the variety of rides they offer. The park has both a water park and theme park, but unlike
+          most combined parks, there hasn't been a cut in the number of rides of both types. Many find it
+           difficult to finish all the rides in one day but come out having the best time. The park has
+            extremely high thrill rides for daredevils and people who like to test their limits. Entry fare: 
+            Changes according to the season. Must try rides: Wavy and vertical fall, Equinox , Insanity,
+             Y-Scream and all other high thrill rides.</p>
+</div>
+ </div>          
         
     )
 }
 
-export default Home;
+export default Home
